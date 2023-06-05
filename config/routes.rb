39c3ab04f2ab1109a779b7a29104e80969375f95
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-06-05 09:24:53 +0200

    add form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'messages/create'
   get 'chatrooms/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :chatrooms, only: :show
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
 end
